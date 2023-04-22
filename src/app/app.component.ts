990d71747c661127f251cedfb514361b6cf7419c
PECHEVARRIA <pechevarria05@gmail.com>
2023-04-21 23:06:45 -0500

    onpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'onpush-change-detection';
+  variable: string;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.variable = "Programmer";
+    this.cdr.detach();
+  }
+
+  ngAfterViewInit(): void {
+
+    this.variable = "Cambios de informacion";
+    //this.cdr.detectChanges();
+    //this.cdr.markForCheck();
+    this.cdr.reattach();
+
+
+  }
+
 }
